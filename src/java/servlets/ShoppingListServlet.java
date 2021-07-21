975800c295658f6e79be4048078690a53f952508
854950 <854950@SAIT227688IT.ACDM.DS.SAIT.CA>
2021-07-21 14:00:50 -0600

    Fourth Commit Completed the list functionality with add and delete

--- src/java/servlets/ShoppingListServlet.java
+++ src/java/servlets/ShoppingListServlet.java
@@ -6,7 +6,7 @@
 package servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -21,36 +21,64 @@ public class ShoppingListServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException { // The doGet() method is used for getting the information from server
+
+        HttpSession session = request.getSession(); // get the session
 
         String loggingout = request.getParameter("logout"); // gets the query string from shoppingList.jsp
         if (loggingout != null) { // if the logout button is clicked
-            HttpSession session = request.getSession(); // get the session
+
             session.invalidate(); // invalidate the session
         }
+        String username = (String) session.getAttribute("username_attribute");
 
-        getServletContext().getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response); // always write this at the end of the doGet
+        if (username != null) {
+            getServletContext().getRequestDispatcher("/WEB-INF/shoppingList.jsp").forward(request, response);
+        } else {
+            getServletContext().getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response); // always write this at the end of the doGet
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException { //  doPost() method is used for sending information to the server.
 
         String action = request.getParameter("action");
 
+        HttpSession session = request.getSession();
+
         if (action != null && action.equals("register")) { // do this three more times - add, delete etc
             String usernameString = request.getParameter("username");
+
             // doPost() first validates that user name and password are not empty. 
             if (usernameString == null || usernameString.equals("")) {
                 request.setAttribute("username", usernameString); // reloads username to boxes
                 getServletContext().getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
                 return;
             } else {
-                HttpSession session = request.getSession();
                 session.setAttribute("username_attribute", usernameString);
+                ArrayList<String> list = new ArrayList<String>();
+                session.setAttribute("shoppingListItem", list);
                 // response.sendRedirect("/WEB-INF/shoppingList.jsp"); // Can't redirect to a JSP, can only use forward
                 getServletContext().getRequestDispatcher("/WEB-INF/shoppingList.jsp").forward(request, response);
             }
+        }
+
+        if (action != null && action.equals("Add")) {
+            String addItemServlet = request.getParameter("item");
+
+            ArrayList<String> list = (ArrayList<String>) session.getAttribute("shoppingListItem");
+            //    request.setAttribute("itemIsAdded", true);
+            list.add(addItemServlet);
+            session.setAttribute("shoppingListItem", list);
+            response.sendRedirect("ShoppingList");
+
+        } else if (action != null && action.equals("Delete")) {
+            String deleteItem = request.getParameter("items");
+            ArrayList<String> list = (ArrayList<String>) session.getAttribute("shoppingListItem");
+            list.remove(deleteItem);
+            session.setAttribute("shoppingListItem", list);
+            response.sendRedirect("ShoppingList");
         }
         //getServletContext().getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
     }
--- web/WEB-INF/shoppingList.jsp
+++ web/WEB-INF/shoppingList.jsp
@@ -1,4 +1,5 @@
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
     <head>
@@ -7,12 +8,21 @@
     </head>
     <body>
         <h1>Shopping List</h1>
-        <form method="post" action="ShoppingList">
+
         <label>Hello, ${username_attribute} <a href="ShoppingList?logout">Log out</a></label>
         <h1>List</h1>
-        <label>Add item:</label>
-        <input type="text" name="items">
-        <input type="submit" value="Add">
+        <form method="post" action="">
+            <label>Add item:</label>
+            <input type="text" name="item">
+            <input type="submit" name="action" value="Add">
+        </form>
+
+        <form method="post" action="">
+            <c:forEach items="${shoppingListItem}" var="items">
+                <input type="radio" name="items" value="${items}">${items}<br>
+            </c:forEach>
+
+            <input type="submit" name="action" value="Delete">
         </form>
     </body>
 </html>
